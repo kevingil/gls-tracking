9ee17ed57f5bc776d1860ca48864a7dab51bdcbc
Kevin Gil <kevin_gil@live.com>
2024-02-13 14:42:47 -0800

    add types to receive GLS API response

--- src/app/components/Packages.tsx
+++ src/app/components/Packages.tsx
@@ -1,9 +1,8 @@
 import { useEffect, useState } from 'react';
+import { TrackShipment } from '../api/gls';
 
 interface GLSDataItem {
-    AccountNumber: number;
     TrackingNumber: string;
-    // Add other properties here based on your data structure
 }
 
 const Packages = () => {
@@ -12,19 +11,11 @@ const Packages = () => {
     useEffect(() => {
         const fetchGLSData = async () => {
             try {
-                const response = await fetch('/api/gls', {
-                    method: 'POST',
-                    // Add any necessary headers or body here
-                    // Example: body: JSON.stringify({ /* request body */ })
-                });
-                if (!response.ok) {
-                    throw new Error('Failed to fetch GLS data');
-                }
-                const data = await response.json();
-                setGlsData(data);
+                const shipmentDate = ''; 
+                const data = await TrackShipment(shipmentDate);
+                setGlsData([data]); 
             } catch (error) {
                 console.error('Error fetching GLS data:', error);
-                // Handle error
             }
         };
 
@@ -33,12 +24,9 @@ const Packages = () => {
 
     return (
         <div>
-            {/* Render your GLS data here */}
             {glsData.map((item) => (
                 <div key={item.TrackingNumber}>
-                    {/* Render each GLS data item */}
                     <p>Tracking Number: {item.TrackingNumber}</p>
-                    {/* Render other properties as needed */}
                 </div>
             ))}
         </div>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,15 +5,13 @@ export default function Home() {
   return (
     <div className="w-full h-full">
       <div className="text-white bg-[#124280] p-4">
-      <p className="text-3xl">GLS Tracking</p>
+        <p className="text-3xl">GLS Tracking</p>
       </div>
       <div className="flex flex-row flex-grow text-black">
-
         <AccountNav />
-         
         <div className="w-full h-full">
-        <p className="text-2xl font-semibold p-4">Shipments</p>
-        <Packages/>
+          <p className="text-2xl font-semibold p-4">Shipments</p>
+          <Packages />
         </div>
       </div>
     </div>
