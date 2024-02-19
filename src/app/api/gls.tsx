e68399ae8255a6488774bf7b64373aee340c866a
Kevin Gil <kevin_gil@live.com>
2024-02-19 14:28:21 -0800

    move server reqs to server comp

---
+++ src/app/api/gls.tsx
@@ -0,0 +1,93 @@
+'use server';
+import { NextApiRequest, NextApiResponse } from 'next';
+
+export type Token = {
+    token: string;
+    expirationTime: number;
+}
+
+export type Shipment = {
+    TrackingNumber: string;
+    TotalCharge: number;
+    ShipDate: string;
+    ServiceCode: string;
+    ShipmentReference: string;
+    DeclaredValue: string;
+    CODValue: string;
+    SpecialInstructions: string;
+    Weight: string;
+    SignatureCode: string;
+    AddnShipperReference1: string;
+    AddnShipperReference2: string;
+    PODImage: string;
+    PODImageMessage: string;
+    ScheduledDeliveryDate: string;
+    ScheduledDeliveryTime: string;
+    DeliveryDate: string;
+    TransitStatus: string;
+    DeliveryTime: string;
+    SignedBy: string;
+    Signature: string;
+    TransitNotes: TransitNote[];
+};
+
+export type TransitNote = {
+    EventDate: string;
+    Comments: string;
+    Location: string;
+};
+
+
+async function fetchToken(h: string, p: string, u: string): Promise<Token> {
+    try {
+        const response = await fetch(`https://${h}/token`, {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json',
+                'Username': p,
+                'Password': u
+            }
+        });
+        if (!response.ok) {
+            console.log(response);
+            throw new Error('Failed to fetch token');
+        }
+
+        const token = response.headers.get('Token');
+        const expirationTime = Date.parse(response.headers.get('TokenExpiry') || '');
+
+        if (!token || isNaN(expirationTime)) {
+            throw new Error('Invalid token');
+        }
+
+        return {
+            token: token,
+            expirationTime: expirationTime
+        };
+    } catch (error) {
+        console.error('Error fetching token:', error);
+        throw new Error('Error fetching token');
+    }
+}
+
+async function trackShipment(h: string, acctnum: string, shipmentDate: string, token: string): Promise<Shipment[]> {
+    try {
+        const response = await fetch(`https://${h}/TrackShipment?AccountNumber=${acctnum}&ShipDate=${shipmentDate}`, {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': `Bearer ${token}`
+            }
+        });
+        if (!response.ok) {
+            throw new Error('Failed to fetch shipment');
+        }
+        const data: Shipment[] = await response.json();
+        return data;
+    } catch (error) {
+        console.error('Error fetching shipment:', error);
+        throw new Error('Error fetching shipment');
+    }
+}
+
+export { fetchToken, trackShipment};
--- src/app/components/Packages.tsx
+++ src/app/components/Packages.tsx
@@ -1,185 +1,41 @@
 'use client';
-
-import { hostname } from 'os';
 import { useEffect, useState } from 'react';
-
-type Recipient = {
-  ShipToCompany: string;
-  ShipToAttention: string;
-  ShipToPhone: string;
-  DeliveryAddress1: string;
-  DeliveryAddress2: string;
-  DeliveryCity: string;
-  DeliveryState: string;
-  DeliveryZip: string;
-  Shipments: Shipment[];
-}
-
-type Shipment = {
-  TrackingNumber: string;
-  TotalCharge: number;
-  ShipDate: string;
-  ServiceCode: string;
-  ShipmentReference: string;
-  DeclaredValue: string;
-  CODValue: string;
-  SpecialInstructions: string;
-  Weight: string;
-  SignatureCode: string;
-  AddnShipperReference1: string;
-  AddnShipperReference2: string;
-  PODImage: string;
-  PODImageMessage: string;
-  ScheduledDeliveryDate: string;
-  ScheduledDeliveryTime: string;
-  DeliveryDate: string;
-  TransitStatus: string;
-  DeliveryTime: string;
-  SignedBy: string;
-  Signature: string;
-  TransitNotes: TransitNote[];
-};
-
-type TransitNote = {
-  EventDate: string;
-  Comments: string;
-  Location: string;
-};
+import { fetchToken, trackShipment, Shipment, Token, TransitNote } from '../api/gls';
 
 interface PackagesProps {
-    shipmentDate: string; 
-}
-
-type Token = {
-    token: string;
-    expirationTime: number;
+    shipmentDate: string;
 }
 
 const Packages = ({ shipmentDate }: PackagesProps) => {
-
-    const username = process.env.NEXT_PUBLIC_GLS_USERNAME;
-    const password = process.env.NEXT_PUBLIC_GLS_PASSWORD;
-    const accountNumber = process.env.NEXT_PUBLIC_GLS_ACCOUNTNUMBER;
-    const host = process.env.NEXT_PUBLIC_GLS_HOST;
-
     const [shipments, setShipments] = useState<Shipment[]>([]);
     const [error, setError] = useState<string | null>(null);
 
-
     useEffect(() => {
-        const fetchShipments = async () => {
-            let token: Token | null = getTokenFromLocalStorage();
-    
-            if (!shipmentDate) {
-                setError('Shipment date is required.');
-                return;
-            }
-    
-            if (!token || isTokenExpired(token)) {
-                if (username && password) {
-                    try {
-                        token = await fetchToken(username, password);
-                        if (token) {
-                            saveTokenToLocalStorage(token.token, 12 * 60 * 60);
-                        } else {
-                            throw new Error('Error prasing token.');
-                        }
-                    } catch (error) {
-                        setError('Error fetching token');
-                        console.error('Error fetching token: ', error);
-                        return;
-                    }
-                } else {
-                    setError('Username and password are required.');
-                    return;
-                }
-            }
-    
+        const fetchData = async () => {
             try {
-                if (token && token.token) {
-                    const data = await TrackShipment(shipmentDate, token.token);
-                    setShipments([data]);
-                    setError(null);
-                } else {
-                    throw new Error('Invalid token.');
-                }
+                const host = process.env.NEXT_PUBLIC_GLS_HOST!;
+                const username = process.env.NEXT_PUBLIC_GLS_USERNAME!;
+                const password = process.env.NEXT_PUBLIC_GLS_PASSWORD!;
+                const accountNumber = process.env.NEXT_PUBLIC_GLS_ACCOUNTNUMBER!;
+                // Fetch token
+                const tokenResponse = await fetchToken(host, password, username);
+                const token = tokenResponse.token;
+
+                // Fetch shipments
+                const shipmentResponse = await trackShipment(host, accountNumber, shipmentDate, token);
+                //This is wrong, use for now
+                const data = shipmentResponse;
+
+                setShipments(data);
+                setError(null);
             } catch (error) {
-                setError('Error tracking shipment');
-                console.error('Error tracking shipment: ', error);
+                setError('Error fetching data');
+                console.error('Error fetching data: ', error);
             }
         };
-    
-        fetchShipments();
+
+        fetchData();
     }, [shipmentDate]);
-    
-
-    function getTokenFromLocalStorage(): Token | null {
-        const tokenData = JSON.parse(localStorage.getItem('gls_token') || 'null');
-        return tokenData;
-    }
-
-    function saveTokenToLocalStorage(token: string, expiresIn: number): void {
-        const expirationTime = new Date().getTime() + expiresIn * 1000;
-        localStorage.setItem('gls_token', JSON.stringify({ token, expirationTime }));
-    }
-
-    function isTokenExpired(tokenData: Token | null): boolean {
-        return !tokenData || tokenData.expirationTime < new Date().getTime();
-    }
-
-    // Fetch the token
-    async function fetchToken(p: string, u: string): Promise<Token> {
-        console.log("Fetch token: " + process.env.NEXT_PUBLIC_GLS_HOST);
-        try {
-            const response = await fetch(`https://${host}/token`, {
-                method: 'GET',
-                headers: {
-                    'Content-Type': 'application/json',
-                    'Username': p,
-                    'Password': u
-                }
-            });
-            if (!response.ok) {
-                throw new Error('Failed to fetch token');
-            }
-
-            const token = response.headers.get('Token');
-            const expirationTime = Date.parse(response.headers.get('TokenExpiry') || '');
-
-            if (!token || isNaN(expirationTime)) {
-                throw new Error('Invalid token');
-            }
-
-            return {
-                token: token,
-                expirationTime: expirationTime
-            };
-        } catch (error) {
-            console.error('Error fetching token:', error);
-            throw new Error('Error fetching token');
-        }
-    }
-
-
-
-    // Track shipment
-    async function TrackShipment(shipmentDate: string, token: string): Promise<Shipment> {
-        console.log("Track shipment");
-        try {
-            const response = await fetch(`https://${process.env.NEXT_PUBLIC_GLS_HOST}/TrackShipment?AccountNumber=${process.env.NEXT_PUBLIC_GLS_ACCOUNTNUMBER}&ShipDate=${shipmentDate}`, {
-                method: 'GET',
-                headers: {
-                    'Content-Type': 'application/json',
-                    'Authorization': `Bearer ${token}`
-                }
-            });
-            const data: Shipment = await response.json();
-            return data;
-        } catch (error) {
-            console.error('Error fetching shipment:', error);
-            throw new Error('Error fetching shipment');
-        }
-    }
 
     return (
         <div>
